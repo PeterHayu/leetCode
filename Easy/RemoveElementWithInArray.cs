9639d371c3ce29a824ddd8107ab8969dd4ac40e7
PeterHayu <hayulegacy@gmail.com>
2020-03-22 17:03:10 -0700

    * remove within Array

---
+++ Easy/RemoveElementWithInArray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy
+{
+    public static class RemoveElementWithInArray
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int i = 0;
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if (nums[j] != val)
+                {
+                    nums[i] = nums[j];
+                    i++;
+                }
+            }
+            return i;
+        }
+    }
+}
