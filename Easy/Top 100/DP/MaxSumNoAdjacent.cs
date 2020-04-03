861da012d92df5cee91bcae66a44d51a0370e49d
PeterHayu <hayulegacy@gmail.com>
2020-04-03 16:45:20 -0700

    * DP template

--- Easy/Top 100/DP/MaxSumNoAdjacent.cs
+++ Easy/Top 100/DP/MaxSumNoAdjacent.cs
@@ -25,16 +25,46 @@ namespace Easy.Top_100
         public static int Rob(int[] nums)
         {
             int length = nums.Length;
-            int a = 0;
-            int b = 0;
+            int even = 0;
+            int odd = 0;
             for (int i = 0; i < length; i++)
             {
                 if (i % 2 == 0)
-                    a = Math.Max(a + nums[i], b);
+                    even = Math.Max(even + nums[i], odd);
                 else
-                    b = Math.Max(b + nums[i], a);
+                    odd = Math.Max(odd + nums[i], even);
             }
-            return Math.Max(a, b);
+            return Math.Max(even, odd);
+        }
+
+        public static int RobDynamicProgrammingTemplate(int[] nums) {
+            int length = nums.Length;
+            if (length <= 0)
+                return 0;
+            var maxArray = new int[nums.Length+1];
+            //improvement can be made to use only two variables instead of the whole array, which track every record
+            maxArray[0] = 0;
+            maxArray[1] = nums[0];
+            for (int i = 1; i < length; i++) {
+                maxArray[i + 1] = Math.Max(maxArray[i], maxArray[i-1]+nums[i]);
+            }
+            return maxArray[length];
+        }
+
+        public static int RobDynamicProgrammingImprove(int[] nums)
+        {
+            int length = nums.Length;
+            if (length <= 0)
+                return 0;
+            var previousMax = 0;
+            var currentMax = nums[0];
+            for (int i = 1; i < length; i++)
+            {
+                int temp = Math.Max(currentMax, previousMax + nums[i]);
+                previousMax = currentMax;
+                currentMax = temp;
+            }
+            return currentMax;
         }
     }
 }
