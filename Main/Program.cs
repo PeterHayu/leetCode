b62a76cd81a81a433b788b54c0b5283706b1a0e6
PeterHayu <hayulegacy@gmail.com>
2020-04-12 21:59:20 -0700

    * merge sorted array

--- Main/Program.cs
+++ Main/Program.cs
@@ -3,6 +3,7 @@ using Easy.Top_100;
 using Easy.Top_100.Array;
 using Easy.Top_100.LinkedList;
 using Easy.Top_100.Tree;
+using Easy.Top_Interviewed.Array;
 using System;
 
 namespace Main
@@ -11,7 +12,10 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(CountAndSay.countAndSay(6));
+            Merge_Sorted_Array.Merge(new[] {4, 5, 6, 0, 0, 0},
+3,
+new[] { 1,2,3},
+3);
         }
     }
 }
