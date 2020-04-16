301d4d27dc4cd5608eff846c7e5b0437200796a5
PeterHayu <hayulegacy@gmail.com>
2020-04-16 11:31:03 -0700

    * tree

---
+++ Easy/Top Interviewed/Tree/Binary Tree Level Order Traversal II.cs
@@ -0,0 +1,28 @@
+﻿using Easy.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy.Top_Interviewed.Tree
+{
+    public class Binary_Tree_Level_Order_Traversal_II
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            var list = new List<IList<int>>();
+            Traverse(list, root, 0);
+            return list;
+        }
+
+        private void Traverse(IList<IList<int>> l, TreeNode root, int lv)
+        {
+            if (root == null)
+                return;
+            if (lv >= l.Count)
+                l.Insert(0, new List<int>());
+            Traverse(l, root.left, lv + 1);
+            Traverse(l, root.right, lv + 1);
+            l[l.Count - lv - 1].Add(root.val);
+        }
+    }
+}
---
+++ Easy/Top Interviewed/Tree/IsSameTree.cs
@@ -0,0 +1,24 @@
+﻿using Easy.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy.Top_Interviewed.Tree
+{
+    class IsSameTree
+    {
+        public bool isSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+                return true;
+            if (p == null || q == null)
+                return false;
+            if (p.val != q.val)
+                return false;
+            //this is wrong - if two node equal we need to continue not return true
+            /*      else
+                       return true; */
+            return IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right);
+        }
+    }
+}
