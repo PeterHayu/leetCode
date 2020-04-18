c1a2851d78be998a32c99d0da8c928b9b597e1dd
PeterHayu <hayulegacy@gmail.com>
2020-04-18 13:52:24 -0700

    * iterative

---
+++ Easy/Top Interviewed/Tree/InOrderTraversal.cs
@@ -0,0 +1,57 @@
+﻿using Easy.Class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Easy.Top_Interviewed.Tree
+{
+    public class InOrderTraversal
+    {
+        //recursive
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            var l = new List<int>();
+            Traverse(root, l);
+            return l;
+        }
+
+        private void Traverse(TreeNode root, IList<int> l)
+        {
+            if (root != null)
+            {
+                Traverse(root.left, l);
+                l.Add(root.val);
+                Traverse(root.right, l);
+            }
+        }
+
+        //iterative
+        public IList<int> InorderTraversalIterative(TreeNode root)
+        {
+            var l = new List<int>();
+            if (root == null)
+                return l;
+            var stack = new Stack<TreeNode>();
+            var curr = root;
+
+            while (curr != null || stack.Any())
+            {
+                //go to the left most branch
+                while (curr != null)
+                {
+                    stack.Push(curr);
+                    curr = curr.left;
+                }
+                //pop the last element first
+                curr = stack.Pop();
+                l.Add(curr.val);
+                curr = curr.right;
+            }
+
+            return l;
+
+        }
+
+    }
+}
