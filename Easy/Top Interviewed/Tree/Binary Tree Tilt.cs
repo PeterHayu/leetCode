dc1235aaea314239c0b05e60249915b1a53ba44d
PeterHayu <hayulegacy@gmail.com>
2020-04-26 00:09:31 -0700

    * root sum

---
+++ Easy/Top Interviewed/Tree/Binary Tree Tilt.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy.Top_Interviewed.Tree
+{
+    public class Binary_Tree_Tilt
+    {
+        int tilt = 0;
+        public int FindTilt(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            Sum(root);
+            return tilt;
+        }
+
+        private int Sum(TreeNode root)
+        {
+            if (root == null)
+                return 0;
+
+            int left = Sum(root.left);
+            int right = Sum(root.right);
+            //exclude this line, its the addtion of all node algorithm
+            tilt += Math.Abs(left - right);
+
+            return root.val + left + right;
+        }
+    }
+}
