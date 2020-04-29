18299ea98453a7f004098e5753c8bcad15ab0215
PeterHayu <hayulegacy@gmail.com>
2020-04-29 12:14:34 -0700

    Update Two Sum BST.cs

--- Easy/Top Interviewed/Tree/Two Sum BST.cs
+++ Easy/Top Interviewed/Tree/Two Sum BST.cs
@@ -6,6 +6,7 @@ namespace Easy.Top_Interviewed.Tree
 {
     public class Two_Sum_BST
     {
+        //BST way
         public bool FindTarget(TreeNode root, int k)
         {
             return dfs(root, root, k);
@@ -34,4 +35,33 @@ namespace Easy.Top_Interviewed.Tree
 
             return (root.val == target) && (root != cur);
         }
+
+
+
+        //Two Sum way
+        public bool FindTarget2(TreeNode root, int k)
+        {
+            if (root == null)
+                return false;
+            var d = new Dictionary<int, TreeNode>();
+            Traverse(root, d);
+            foreach (var c in d.Keys)
+            {
+                var diff = k - c;
+                if (d.ContainsKey(diff) && d[diff] != d[c])
+                    return true;
+            }
+            return false;
+        }
+
+        public void Traverse(TreeNode root, Dictionary<int, TreeNode> d)
+        {
+            if (root == null)
+                return;
+
+            d[root.val] = root;
+            Traverse(root.left, d);
+            Traverse(root.right, d);
+        }
     }
+}
