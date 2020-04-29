0176557de34a8c301c9fc9a06e892de5149563ed
PeterHayu <hayulegacy@gmail.com>
2020-04-28 21:48:11 -0700

    Create Two Sum BST.cs

---
+++ Easy/Top Interviewed/Tree/Two Sum BST.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easy.Top_Interviewed.Tree
+{
+    public class Two_Sum_BST
+    {
+        public bool FindTarget(TreeNode root, int k)
+        {
+            return dfs(root, root, k);
+        }
+
+        private bool dfs(TreeNode root, TreeNode cur, int k)
+        {
+            if (cur == null)
+                return false;
+            var target = k - cur.val;
+
+            return SearchBST(root, cur, target) || dfs(root, cur.left, k) || dfs(root, cur.right, k);
+
+        }
+
+
+        private bool SearchBST(TreeNode root, TreeNode cur, int target)
+        {
+            if (root == null)
+                return false;
+
+            if (root.val < target)
+                return SearchBST(root.right, cur, target);
+            if (root.val > target)
+                return SearchBST(root.left, cur, target);
+
+            return (root.val == target) && (root != cur);
+        }
+    }
