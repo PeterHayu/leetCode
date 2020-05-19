3036c9fa75fafaf7ba555af11994c5109ad591fe
PeterHayu <hayulegacy@gmail.com>
2020-05-19 12:27:54 -0700

    * add new solution to binary search examples

---
+++ Medium/Binary Search/Find First and Last Position of Element in Sorted Array.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium.Binary_Search
+{
+    class Find_First_and_Last_Position_of_Element_in_Sorted_Array { 
+
+    //O(N) solution
+     public int[] SearchRange(int[] nums, int target)
+    {
+        bool isFirst = true;
+        int first = -1, last = -1;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == target)
+            {
+                if (isFirst)
+                {
+                    first = i;
+                    isFirst = false;
+                }
+                last = i;
+            }
+        }
+        return new int[] { first, last };
+    }
+}
+}
