c39c4206d0da036620bc1843fcc5a9320e7989e1
PeterHayu <hayulegacy@gmail.com>
2020-05-20 21:57:17 -0700

    Create Permutation.cs

---
+++ Medium/Back Tracking/Permutation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium.Back_Tracking
+{
+    class Permutation
+    {
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            BackTrack(result, new List<int>(), nums);
+            return result;
+        }
+
+        private void BackTrack(List<IList<int>> result, List<int> path, int[] nums)
+        {
+            if (path.Count == nums.Length)
+            {
+                result.Add(new List<int>(path));
+                return;
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (path.Contains(nums[i])) continue;
+                path.Add(nums[i]);
+                BackTrack(result, path, nums);
+                path.RemoveAt(path.Count - 1);
+            }
+
+        }
+    }
+}
