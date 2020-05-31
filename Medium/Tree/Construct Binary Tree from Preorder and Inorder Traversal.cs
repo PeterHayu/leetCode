fbb640a7a8ab07981980b75093f485c894e28426
PeterHayu <hayulegacy@gmail.com>
2020-05-30 22:46:22 -0700

    * tree problems

---
+++ Medium/Tree/Construct Binary Tree from Preorder and Inorder Traversal.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium.Tree
+{
+    class Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal
+    {
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+            return Build(preorder, inorder);
+        }
+
+        private TreeNode Build(int[] preorder, int[] inorder)
+        {
+            if (preorder.Length <= 0 | inorder.Length <= 0)
+                return null;
+
+            var result = new TreeNode(preorder[0]);
+            int rootIndex = 0;
+
+            for (int i = 0; i < inorder.Length; i++)
+            {
+                if (inorder[i] == result.val)
+                {
+                    rootIndex = i;
+                    break;
+                }
+            }
+
+            result.left = Build(preorder[1..^0], inorder[0..(rootIndex)]);
+            result.right = Build(preorder[(rootIndex + 1)..^0], inorder[(rootIndex + 1)..^0]);
+
+            return result;
+        }
+    }
+}
---
+++ Medium/Tree/Flatten Binary Tree to Linked List.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium.Tree
+{
+    class Flatten_Binary_Tree_to_Linked_List
+    {
+        public void Flatten(TreeNode root)
+        {
+            Traverse(root, null);
+        }
+
+        private TreeNode Traverse(TreeNode root, TreeNode prev)
+        {
+            if (root == null)
+                return prev;
+            prev = Traverse(root.right, prev);
+            prev = Traverse(root.left, prev);
+            root.right = prev;
+            root.left = null;
+            //prev=root;
+            return root;
+        }
+    }
+}
