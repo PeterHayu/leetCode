9a501522aaee92119948e5298056512816ac11a5
PeterHayu <hayulegacy@gmail.com>
2020-06-22 22:37:48 -0700

    Create Bit Manipulation Count numbers of 1s.cs

---
+++ Medium/Bit Manipulation Count numbers of 1s.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium
+{
+    class Bit_Manipulation_Count_numbers_of_1s
+    {
+        public int HammingWeight(uint n)
+        {
+            int result = 0;
+            while (n != 0)
+            {
+                result++;
+                n &= n - 1;
+            }
+            return result;
+        }
+
+    }
+}
