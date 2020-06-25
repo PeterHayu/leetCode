37ccb38be559982767518cda58226d0ef3a8fd0b
PeterHayu <hayulegacy@gmail.com>
2020-06-25 11:56:35 -0700

    * grid iteration

---
+++ Medium/Iterate Grid/BFS/Rotten Oranges.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium.Iterate_Grid.BFS
+{
+    class Rotten_Oranges
+    {
+        public int OrangesRotting(int[][] grid)
+        {
+            if (grid == null || grid.Length == 0) return 0;
+            int rows = grid.Length;
+            int cols = grid[0].Length;
+            Queue<int[]> queue = new Queue<int[]>();
+            int count_fresh = 0;
+            //1, iterate through the grid and enqueue all rotten oranges
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (grid[i][j] == 2)
+                    {
+                        //Put the position of all rotten oranges in queue
+                        queue.Enqueue(new int[] { i, j });
+                    }
+                    else if (grid[i][j] == 1)
+                    {
+                        //count the number of fresh oranges
+                        count_fresh++;
+                    }
+                }
+            }
+
+
+            //if count of fresh oranges is zero --> return 0 
+            if (count_fresh == 0) return 0;
+
+            //same as counting tree level
+            int count = 0;
+            //four directions
+            var dirs = new int[][]{new int[]{1,0},
+                               new int[]{-1,0},
+                               new int[]{0,1},
+                               new int[]{0,-1}};
+            //bfs starting from initially rotten oranges
+            while (queue.Count != 0)
+            {
+                count++;
+                int size = queue.Count;
+                for (int i = 0; i < size; i++)
+                {
+                    int[] point = queue.Dequeue();
+                    //iterate through 4 directions of a rotten orange
+                    foreach (var dir in dirs)
+                    {
+                        int x = point[0] + dir[0];
+                        int y = point[1] + dir[1];
+                        //if x or y is out of bound
+                        //or the orange at (x , y) is already rotten
+                        //or the cell at (x , y) is empty
+                        //we do nothing
+                        if (x < 0 || y < 0 || x >= rows || y >= cols || grid[x][y] == 0 || grid[x][y] == 2) continue;
+                        //mark the orange at (x , y) as rotten
+                        grid[x][y] = 2;
+                        //put the new rotten orange at (x , y) in queue
+                        queue.Enqueue(new int[] { x, y });
+                        //decrease the count of fresh oranges by 1
+                        count_fresh--;
+                    }
+                }
+            }
+            return count_fresh == 0 ? count - 1 : -1;
+        }
+    }
+}
---
+++ Medium/Iterate Grid/DFS/Number of Islands.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium.Iterate_Grid.DFS
+{
+    class Number_of_Islands
+    {
+        public int NumIslands(char[][] grid)
+        {
+            if (grid.Length < 1)
+                return 0;
+            int result = 0;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == '1')
+                    {
+                        Traverse(grid, i, j);
+                        result++;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private void Traverse(char[][] grid, int i, int j)
+        {
+            if (i < 0 || i >= grid.Length || j < 0 || j >= grid[0].Length || grid[i][j] != '1')
+                return;
+
+            //visited
+            grid[i][j] = '0';
+
+            Traverse(grid, i, j + 1);
+            Traverse(grid, i + 1, j);
+            Traverse(grid, i - 1, j);
+            Traverse(grid, i, j - 1);
+        }
+
+
+
+    }
+}
