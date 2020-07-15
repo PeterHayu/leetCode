a7001ed4e64e074f963c07df349f77edcd16d8e5
PeterHayu <hayulegacy@gmail.com>
2020-07-14 22:19:36 -0700

    * level order traversal

--- Medium/Class/Node.cs
+++ Medium/Class/Node.cs
@@ -9,12 +9,24 @@ namespace Medium
             public int val;
             public Node next;
             public Node random;
+            public Node left;
+            public Node right;
 
-            public Node(int _val)
+        public Node() { }
+
+        public Node(int _val)
             {
                 val = _val;
                 next = null;
                 random = null;
-            } 
+            }
+
+        public Node(int _val, Node _left, Node _right, Node _next)
+        {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
     }
 }
---
+++ Medium/Tree/Connect Level Order Nodes in Pefect Binary Tree.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium.Tree
+{
+    class Connect_Level_Order_Nodes_in_Pefect_Binary_Tree
+    {
+        public Node Connect(Node root)
+        {
+            //preorder traversal
+            Traverse(root);
+            return root;
+        }
+
+        private void Traverse(Node root)
+        {
+
+            //empty tree
+            if (root == null)
+                return;
+
+            //reach leaf 
+            if (root.left == null && root.right == null)
+                return;
+
+            //since perfect binary, we dont need to check null for left and right children
+            //simply connect left and right children from root
+            //example: connect 2 to 3
+            //add left check here if not perfect binary tree
+            root.left.next = root.right;
+
+            //second time visited, one of the sub root on left sub tree
+            //example: connect 5 to 6
+            //add right check here if not perfect binary tree
+            if (root.next != null)
+            {
+                root.right.next = root.next.left;
+            }
+
+            Connect(root.left);
+            Connect(root.right);
+        }
+
+        public Node Connect(Node root)
+        {
+            if (root == null)
+                return null;
+
+            // Run level by level order traversal
+            // set a previous node in the same level
+            var queue = new Queue<Node>();
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                var size = queue.Count;
+
+                Node previous = null;
+                for (int i = 0; i < size; i++)
+                {
+                    var node = queue.Dequeue();
+                    if (node.left != null)
+                        queue.Enqueue(node.left);
+                    if (node.right != null)
+                        queue.Enqueue(node.right);
+
+                    if (previous != null)
+                        previous.next = node;
+
+                    previous = node;
+                }
+            }
+
+            return root;
+        }
+    }
+}
