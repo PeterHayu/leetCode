6de550ebf7493cf9844996902fe1e9a634766bdf
PeterHayu <hayulegacy@gmail.com>
2020-07-24 21:12:19 -0700

    Create Sort Int String To Join Largest Number.cs

---
+++ Medium/Sort Int String To Join Largest Number.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Medium
+{
+    class Sort_Int_String_To_Join_Largest_Number
+    {
+        public string LargestNumber(int[] nums)
+        {
+            System.Array.Sort(nums, (a, b) => (b + "" + a).CompareTo(a + "" + b));
+            return (nums[0] == 0) ? "0" : String.Join("", nums);
+        }
+    }
+}
