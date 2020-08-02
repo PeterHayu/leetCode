4f4e0396f61d276310172cda5bfdda6009a3f075
PeterHayu <hayulegacy@gmail.com>
2020-08-01 22:32:44 -0700

    * add two problems

---
+++ Hard/Merge K List.cs
@@ -0,0 +1,60 @@
+﻿using Main;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hard
+{
+    class Merge_K_List
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            if (lists.Length == 0)
+                return null;
+            //merge 1,2 to 1, 3,4 to 3, 
+            for (int i = 1; i < lists.Length; i *= 2)
+            {
+                for (int j = 0; i + j < lists.Length; j += i * 2)
+                {
+                    lists[j] = MergeTwoLists(lists[j], lists[i + j]);
+                }
+            }
+
+            return lists[0];
+        }
+
+        //exactly the same as Merge Two Linked List
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            //creating two pointer pointing to a same new list with one element, 0
+            ListNode l3, result = new ListNode(0);
+            //l3 and result both pointing to same location
+            l3 = result;
+            //while l1 and l2 pointer havent reach the end of their list
+            while (l1 != null & l2 != null)
+            {
+                //if the value l1 points to less then l2
+                if (l1.val < l2.val)
+                {
+                    //l3 should link to l1
+                    l3.next = l1;
+                    //l1 move fwd
+                    l1 = l1.next;
+                }
+                else
+                {
+                    l3.next = l2;
+                    l2 = l2.next;
+                }
+                //l3 pointer move fwd
+                l3 = l3.next;
+            }
+            //when loop breaks, l1 reaches the end
+            if (l1 == null)
+                l3.next = l2; //l3 link to l2
+            if (l2 == null)
+                l3.next = l1;
+            return result.next; //skip the current value
+        }
+    }
+}
---
+++ Hard/Regular Expression Matching - DP.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hard
+{
+    class Regular_Expression_Matching___DP
+    {
+        public bool IsMatch(string s, string p)
+        {
+
+            //dp means: 0-i of s matches 0-j of p
+            var dp = new bool[s.Length + 1, p.Length + 1];
+            dp[0, 0] = true;
+
+
+            //scenario: * and deleted (aab, c*a*b) = true
+            for (int j = 0; j < p.Length; j++)
+            {
+                //if from beginning, s match some following part of p after *
+                if (p[j] == '*' && dp[0, j - 1])
+                {
+                    //mark it s matches p[0..i+1] (following part of p after *)
+                    dp[0, j + 1] = true;
+                }
+            }
+
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = 0; j < p.Length; j++)
+                {
+                    //if matches directly, set as true
+                    if (s[i] == p[j])
+                        dp[i + 1, j + 1] = dp[i, j];
+                    //if find . (can be anything), set as true
+                    if (p[j] == '.')
+                        dp[i + 1, j + 1] = dp[i, j];
+                    if (p[j] == '*')
+                    {
+                        //if not .*,  (baa, bb*aa) = true because * and delete scenario
+                        //this cannot be combined with leading * and delete scenario 
+                        if (s[i] != p[j - 1] && p[j - 1] != '.')
+                        {
+                            //compare the first b, if they match means can be deleted
+                            dp[i + 1, j + 1] = dp[i + 1, j - 1];
+                        }
+                        else
+                        {
+                            //sceanrio: a* && aa(add), aaa && ab*a*c*a"(delete)
+                            dp[i + 1, j + 1] = dp[i, j + 1] || dp[i + 1, j - 1];
+                        }
+                    }
+                }
+            }
+
+
+            return dp[s.Length, p.Length];
+
+        }
+    }
+}
