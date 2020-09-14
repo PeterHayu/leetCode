f0895335250d8894c5c5295c667a822b9a317e03
peterhayu <hayuelgacy@gmail.com>
2020-09-13 22:51:28 -0700

    * update binary search

--- Main/Binary Search Template.cs
+++ Main/Binary Search Template.cs
@@ -30,6 +30,7 @@ namespace Main
             return -1;
         }
 
+        //how many elements are stritcly smaller than target?
         int left_bound(int[] nums, int target)
         {
             int left = 0, right = nums.Length - 1;
@@ -50,13 +51,13 @@ namespace Main
                     right = mid - 1;
                 }
             }
-            // 最后要检查 left 越界的情况
+            // 最后要检查 left 越界的情况 (left + 1 might > length if left greater than all)
             if (left >= nums.Length || nums[left] != target)
                 return -1;
             return left;
         }
 
-
+        //how many elements are stritcly larger than target?
         int right_bound(int[] nums, int target)
         {
             int left = 0, right = nums.Length - 1;
@@ -77,10 +78,37 @@ namespace Main
                     left = mid + 1;
                 }
             }
-            // 最后要检查 right 越界的情况
+            // 最后要检查 right 越界的情况 (right -1 < 0 if right smaller than all)
             if (right < 0 || nums[right] != target)
                 return -1;
             return right;
         }
+
+        int BinarySearchRecursive(int[] arr, int l, int r, int x)
+        {
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+
+                // If the element is present at the middle 
+                // itself 
+                if (arr[mid] == x)
+                    return mid;
+
+                // If element is smaller than mid, then 
+                // it can only be present in left subarray 
+                if (arr[mid] > x)
+                    return BinarySearchRecursive(arr, l, mid - 1, x);
+
+                // Else the element can only be present 
+                // in right subarray 
+                return BinarySearchRecursive(arr, mid + 1, r, x);
+            }
+
+            // We reach here when element is not 
+            // present in array 
+            return -1;
+        }
+
     }
 }
---
+++ Main/Sorting Algorithms.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Main
+{
+    public class Sorting_Algorithms
+    {
+        public void bubbleSort(int[] arr, int n)
+        {
+            int i, j;
+            bool isSwap = false;
+            for (i = 0; i < n - 1; i++)
+            { 
+                // Last i elements are already in place  
+                for (j = 0; j < n - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        swap(arr, arr[j], arr[j + 1]);
+                        isSwap = true;
+                    }
+                }
+                if (!isSwap)
+                    break;
+            }
+        }
+
+
+        void swap(int[] array, int from, int to)
+        {
+            int temp = array[from]; array[from] = array[to]; array[to] = temp;
+        }
+
+    }
+}
