eb3f660abc22fd4245591bea159b826dab4db053
Haoshen Yu <hayulegacy@gmail.com>
2020-09-28 14:13:34 -0700

    * power of 3

---
+++ Easy/Top Interviewed/Mathmetics/Power Of Three.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Easy.TopInterviewed.Mathmetics
+{
+    public class Power_Of_Three
+    {
+        //check if a number is of power of 3
+        public bool IsPowerOfThree(int n)
+        {
+            if (n > 1)  // n = 0 edge case
+                while (n % 3 == 0)
+                    n /= 3;
+            return n == 1;
+        }
+    }
+}
