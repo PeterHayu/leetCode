d0b53ff2b8162ff0a4f10b4ebf1b02e37fa895cf
Haoshen Yu <hayulegacy@gmail.com>
2020-09-28 15:43:04 -0700

    * add

---
+++ Medium/Array/Product Of Array Except Self.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Medium.Array
+{
+    public class Product_Of_Array_Except_Self
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            var result = new int[nums.Length];
+            System.Array.Fill(result, 1);
+            int left = 1, right = 1;
+
+            //result[i] represents product of all the elements to the left of element at index i.
+            //for i =1,  L[i] = nums[i - 1] * L[i - 1];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result[i] *= left;
+                left *= nums[i];
+            }
+
+            //now, we multiply the  product of all the elements to the left of element at index i.
+            //for i = length-2, R[i+1] = nums[i + 1] * R[i + 1];
+            //this process save a loop to multiply them together result[i] = L[i] * R[i]
+            for (int j = nums.Length - 1; j >= 0; j--)
+            {
+                result[j] *= right;
+                right *= nums[j];
+            }
+
+            return result;
+        }
+
+    }
+}
---
+++ Medium/Iterate Grid/Neither/Set Matrix Zeroes.cs
@@ -0,0 +1,100 @@
+﻿using System;
+namespace Medium.IterateGrid.Neither
+{
+    public class Set_Matrix_Zeroes
+    {
+        public void SetZeroes(int[][] matrix)
+        {
+            if (matrix.Length == 0 || matrix[0].Length == 0)
+                return;
+
+            //make the first column and first row as row0 and col0 indicator
+            //however, this will screw up the first column and first row
+            //bc we cannot confirm if it has zero from the beginning
+            //or being updated with indicator 0
+            //lets define two variable
+            var col0 = false;
+            var row0 = false;
+            var n = matrix.Length;
+            var m = matrix[0].Length;
+
+            //check if initial row has zero
+            for (int i = 0; i < n; i++)
+            {
+                if (matrix[i][0] == 0)
+                {
+                    row0 = true;
+                    break;
+                }
+            }
+
+            //check if initial column has zero
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[0][j] == 0)
+                {
+                    col0 = true;
+                    break;
+                }
+            }
+
+            //first, handle except the first col and row
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < m; j++)
+                {
+                    if (matrix[i][j] == 0)
+                    {
+                        //safely mark as 0 as the indicator
+                        //however this indicator should not lead to first rol/col to be zero, if col0/row0=false
+                        matrix[0][j] = 0;
+                        matrix[i][0] = 0;
+                    }
+                }
+            }
+
+            //change the rows except first row
+            for (int i = 1; i < n; i++)
+            {
+                if (matrix[i][0] == 0)
+                {
+                    for (int j = 1; j < m; j++)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+
+            //change the column except first column
+            for (int j = 1; j < m; j++)
+            {
+                if (matrix[0][j] == 0)
+                {
+                    for (int i = 1; i < n; i++)
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+            //safely mark the first row as all 0s
+            if (row0)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    matrix[i][0] = 0;
+                }
+            }
+
+            //safely mark the first col as all 0s
+            if (col0)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+    }
+}
