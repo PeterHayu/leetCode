4e1179969743d8fb045bdfb6f75274a7dc47b109
Haoshen Yu <hayulegacy@gmail.com>
2020-09-30 17:45:59 -0700

    * add more comments

---
+++ Medium/Maximal Square.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Medium
+{
+    public class Maximal_Square
+    {
+        public int MaximalSquare(char[][] matrix)
+        {
+            if (matrix.Length < 0)
+                return -1;
+            //dp is the max size of 1 squares that right down corner located
+            int max = -1;
+            var dp = new int[matrix.Length, matrix[0].Length];
+
+            //fill the dp matrix
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                dp[i, 0] = matrix[i][0] == '1' ? 1 : 0;
+                max = Math.Max(max, dp[i, 0]);
+            }
+
+            for (int j = 0; j < matrix[0].Length; j++)
+            {
+                dp[0, j] = matrix[0][j] == '1' ? 1 : 0;
+                max = Math.Max(max, dp[0, j]);
+            }
+
+
+            for (int i = 1; i < matrix.Length; i++)
+            {
+                for (int j = 1; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] == '1')
+                    {
+                        //dp[i][j] = Min(left,upper,diagonal left upper)
+                        dp[i, j] = Math.Min(dp[i - 1, j - 1], Math.Min(dp[i - 1, j], dp[i, j - 1])) + 1;
+                        max = Math.Max(max, dp[i, j]);
+                    }
+                }
+            }
+
+            return max * max;
+        }
+
+    }
+}
--- Medium/Tree/LCR for Binary Tree.cs
+++ Medium/Tree/LCR for Binary Tree.cs
@@ -3,6 +3,7 @@ namespace Medium.Tree
 {
     public class LCR_for_Binary_Tree
     {
+        //both O(N)
         public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
         {
             if (root == null)
@@ -10,13 +11,14 @@ namespace Medium.Tree
             //when we found the node, return up to root of the tree
             if (root == p || root == q)
                 return root;
-            //recursively go to left and right
+            //recursively go to left and right to find p or q. If found it will be a node, if not its null
+            //use this to identify which sides p and q are on
             var left = LowestCommonAncestor(root.left, p, q);
             var right = LowestCommonAncestor(root.right, p, q);
-            //when p and q are on different side of a tree, return the LCR of the tree
+            //when p and q are on different side of a tree, return root of the tree
             if (left != null && right != null)
                 return root;
-            //when p and q are on same side of tree, return root of the tree
+            //when p and q are on same side of tree, return the LCR of the tree, which is the root of either p or q, whichever on a higher level 
             if (left == null)
                 return right;
             if (right == null)
@@ -27,4 +29,4 @@ namespace Medium.Tree
 
     }
 }
-}
+
