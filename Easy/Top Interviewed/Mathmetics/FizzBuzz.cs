d19c18d43016b81903c31b5d3519685cf3d806c2
Haoshen Yu <hayulegacy@gmail.com>
2020-12-16 21:55:06 -0800

    * add two problems

---
+++ Easy/Top Interviewed/Mathmetics/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Easy.TopInterviewed.Mathmetics
+{
+    public class FizzBuzz
+    {
+        public IList<string> FizzBuzz2(int n)
+        {
+            var ans = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int m = i + 1;
+                if (i < 2)
+                    ans.Add(m + "");
+                else
+                {
+                    if (m % 3 == 0 || m % 5 == 0)
+                    {
+                        if (m % 3 == 0 && m % 5 == 0)
+                            ans.Add("FizzBuzz");
+                        else if (m % 3 == 0)
+                            ans.Add("Fizz");
+                        else if (m % 5 == 0)
+                            ans.Add("Buzz");
+                    }
+                    else
+                        ans.Add(m + "");
+                }
+
+            }
+            return ans;
+        }
+    }
+}
---
+++ Medium/Array/Shuffle An Array.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Medium.Array
+{
+    public class Shuffle_An_Array
+    {
+        private Random ran;
+        private int l;
+        private int[] originalA;
+
+        public Shuffle_An_Array(int[] nums)
+        {
+            ran = new Random();
+            l = nums.Length;
+            originalA = (int[])nums.Clone();
+        }
+
+        /** Resets the array to its original configuration and return it. */
+        public int[] Reset()
+        {
+            return originalA;
+        }
+
+        /** Returns a random shuffling of the array. */
+        public int[] Shuffle()
+        {
+            var ans = new int[l];
+            //randomly pick a number from the rest of the array
+            //then swap value 
+            for (int i = 0; i < originalA.Length; i++)
+            {
+                int r = ran.Next(i + 1);
+                //this step pick a previous value from the array
+                //and randomly swap with it
+                ans[i] = ans[r];
+                ans[r] = originalA[i];
+            }
+            return ans;
+        }
+    }
+
+
+
+
+}
+
